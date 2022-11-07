dc280d652ba72387bc639361c3d06daace841006
tomona-0311 <karafuru0311@gmail.com>
2022-11-07 15:51:50 +0900

    Roomのdbの整合性

--- app/models/room.rb
+++
@@ -1,4 +0,0 @@
-class Room < ApplicationRecord
-  mount_uploader :avatar, AvatarUploader
-belongs_to:reservation
-end
---
+++ db/migrate/20221107052359_user_id.rb
@@ -0,0 +1,4 @@
+class UserId < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
